780fe0abceb6770f44b15f2179413f24caab249c
mariacc2206 <mariadino2009@gmail.com>
2025-09-30 12:08:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -20,6 +20,31 @@ function iniciarjogo() {
     // adicionamos 1  para ter entre 1 é 100.00...
     // math.floor() arredonda para o menor inteiro, então teremos entre 1 e 100.
     numeroSecreto = Math.floor(Math.rondo() * 100) + 1;
-    console.log('Numero Secreto (apenas para testes): , Numero Secreto)
-        
+    console.log('Numero Secreto (apenas para testes):' , Numero Secreto); // ajuda no debug!
+
+
+    //resetar o contador de chances 
+    contadorDeChances = 1;
+    chacesrestantesElement.textContent = MAX_CHANCHES; // mostra o total de chances
+
+    //limpar as mensagens anteriores
+    mensagemElement.textContent = '';
+    palpitesAnterioresElements.textContent = '';
+    dicaElement.textContent = '';
+    listaDePalpites = [];
+
+
+    mensagemElement.style.color = '#f0f0f0';// cor padrao do texto
+    dicaElement.style.color = '#ffeb3b'; // cor padrao da dica 
+
+    // reativar o campo de palpite e o botao de enviar
+    campoPalpite.disabled = false;
+    botaoEnviarPalpites.disabled = false;
+    campoPalpite.value = ''; // limpa o campo de entrada
+    campoPalpite.focus(); // coloca o cursor no campo para o usuario digitar 
+
+    // esconder o botao de reiniciar
+    botaoReniciar.classlist.add('hidden');
+
+
 }
