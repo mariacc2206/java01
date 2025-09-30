c3d3ed6aa581aa902428a0534d9951c289fb5eac
mariacc2206 <mariadino2009@gmail.com>
2025-09-30 11:44:20 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,18 @@
+//arrays ->vetores, matrizes 
+// estrutura de dados para armazenar um conjunto de elementos
+
+let frutas = ["banana", "maça", "melancia"]; //array de tamanho 3
+console.log(frutas);
+console.log(frutas[1]);
+
+let numeros = new Array(2,8,9,10);
+console.log(numeros);
+
+let arrayVazio = [];
+console.log(arrayVazio);
+arrayVazio[0] = "Clara"
+console.log(arrayVazio);
+
+//Tamanho do array -> length 
+let dados = [10,50,30]; //tamanho 3 -> length 3
+console.log(dados.length);
